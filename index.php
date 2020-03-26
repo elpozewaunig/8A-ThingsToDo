a7c67d70a67920a10ce0421e4d75d35e53dce29b
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2020-03-26 10:21:43 +0100

    Return to login if password is wrong

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php 
 session_start();
 
+if (isset($_SESSION['return_to_login'])) {
+  if($_SESSION['return_to_login']) { // checks if the page was redirected by login.php
+    unset($_SESSION['return_to_login']);
+    $refresh = false;
+  }
+}
+else {
+  $refresh = true;
+}
+
 if (isset($_SESSION['login_successful'])) {
   if ($_SESSION['login_successful']) {
     header("Location: main.php");
     exit();
+  }
+  elseif ($_SESSION['login_successful'] == false) {
+    if ($refresh) {
+      unset($_SESSION['login_successful']); // delete information that the password was false
+    }
   }
 }
  ?>
@@ -34,7 +49,21 @@ if (isset($_SESSION['login_successful'])) {
   <h3>Login here first</h3>
   
 <form action="login.php" method="post">
-      <input type="password" name="password"><br>
+  
+  <?php
+  
+  if(isset($_SESSION['login_successful'])) {
+    if ($_SESSION['login_successful'] == false) {
+      echo "<input class=\"wrong-input\" type=\"password\" name=\"password\"><br>";
+      echo "<div class=\"alert\"> Password is incorrect. Try again </div><br>";
+    }
+  }
+  else {
+    echo "<input type=\"password\" name=\"password\"><br>";
+  }
+      
+  ?>
+      
       <input type="submit" value="Log in">
     </form>
     
--- login.php
+++ login.php
@@ -13,6 +13,9 @@ if ($password == $key) {
   }
   else {
     echo "Wrong password";
+    header("Location: index.php");
+    $_SESSION['login_successful'] = false;
+    $_SESSION['return_to_login'] = true; // tells login screen this is no refresh
   }
 
 ?>
