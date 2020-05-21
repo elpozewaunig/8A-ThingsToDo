e43598f4af67f771b40a598e1a6510aae8e39e9e
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2020-05-22 00:59:49 +0200

    Fixed versionification of absolute file paths

--- versionify.php
+++ versionify.php
@@ -1,9 +1,11 @@
 <?php
 
 function versionify($path) {
+  
+  $checkable_path = trim($path, '/'); // necessary to trim slash, since PHP will always route from document root
 
-  if(file_exists($path)) {
-    return $path."?version=".filemtime($path);
+  if(file_exists($checkable_path)) {
+    return $path."?version=".filemtime($checkable_path);
   }
   else {
     return false;
