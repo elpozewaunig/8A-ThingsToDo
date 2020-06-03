62f71872efd88e43be1c19de33ba94bd7bd8715f
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2020-06-03 17:17:28 +0200

    Made styling fixes and added WPF-INF

--- constants.php
+++ constants.php
@@ -1,5 +1,5 @@
 <?php
 
-const SUBJECTS = ["KV", "M", "D", "E", "GSPB", "GWK", "BIUK", "PH", "ME", "PHL", "BE", "L", "IT", "CAE", "SPA", "REK", "REE", "BSPK", "BSPM", "INFG-SR", "INFG-KAM", "INFW-ROH", "INFW-MI", "PUP", "WPF-ME", "WPF-GSPB", "WPF-BIUK", "WPF-SPK"];
+const SUBJECTS = ["KV", "M", "D", "E", "GSPB", "GWK", "BIUK", "PH", "ME", "PHL", "BE", "L", "IT", "CAE", "SPA", "REK", "REE", "BSPK", "BSPM", "INFG-SR", "INFG-KAM", "INFW-ROH", "INFW-MI", "PUP", "WPF-ME", "WPF-INF", "WPF-GSPB", "WPF-BIUK", "WPF-SPK"];
 
 ?>
