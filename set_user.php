c324b48f46e911fb60cff82c55ffdfd313bb2444
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2020-06-15 21:22:48 +0200

    Don't unset user if set_user.php is not given a POST request

--- set_user.php
+++ set_user.php
@@ -1,8 +1,10 @@
 <?php
 session_start(); 
 
-$user = $_POST['user'];
-setcookie("user", $user);
+if(isset($_POST['user'])) {
+  $user = $_POST['user'];
+  setcookie("user", $user);
+}
 
 if (isset($_SESSION['origin'])) {
   header("Location: ".$_SESSION['origin']);
