1d7e3b53f0ca55e0ab7ebd1f651aff7cea76390a
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2020-06-20 22:49:26 +0200

    Don't let progress for non-existing users be written if the user cookie is manipulated

--- push_progress.php
+++ push_progress.php
@@ -1,12 +1,17 @@
-<?php 
+<?php
+
+include 'modules/userlist_build.php';
+
+$user_array = get_users(); 
 
 if (isset($_POST['progress'])) {
+  if (isset($_COOKIE['user']) && in_array($_COOKIE['user'], $user_array)) { // checks if user is valid
+    $progress = $_POST['progress'];
+    $user = $_COOKIE['user'];
 
-  $progress = $_POST['progress'];
-  $user = $_COOKIE['user'];
-
-  $handle = fopen("data/progress/$user", "w");
-  fwrite($handle, implode(',', $progress));
+    $handle = fopen("data/progress/$user", "w");
+    fwrite($handle, implode(',', $progress));
+  }
 }
 
 header("Location: main.php");
