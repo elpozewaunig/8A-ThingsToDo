3dca3bb5827ebc3705a9d677f6cdfef1c43f12f3
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2020-11-15 12:58:30 +0100

    Added past conferences to generated ics file

--- ics.php
+++ ics.php
@@ -5,6 +5,13 @@ include 'modules/icalify_conferences.php';
 header('Content-Type: text/calendar');
 
 $conferences = file('data/conferences/conferences.txt');
+if(file_exists('data/conferences/archive.txt')) {
+  $archive = file('data/conferences/archive.txt');
+  $data = array_merge($conferences, $archive);
+}
+else {
+  $data = $conferences;
+}
 
 if(isset($_GET['user']) && valid_user($_GET['user'])) {
   $user = $_GET['user'];
@@ -12,5 +19,5 @@ if(isset($_GET['user']) && valid_user($_GET['user'])) {
 else {
   $user = "all";
 }
-echo icalify_conferences($conferences, $user);
+echo icalify_conferences($data, $user);
 ?>
