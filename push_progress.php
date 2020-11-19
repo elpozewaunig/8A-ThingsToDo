b544772ac207abf9bf250e643c3e9b774275050d
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2020-11-19 20:27:32 +0100

    Set user cookie to respective user after saving progress
    This avoids confusion when another user was selected in another tab

--- push_progress.php
+++ push_progress.php
@@ -8,13 +8,14 @@ $id_array = get_work_ids();
 
 if ( isset($_POST['progress']) && empty(array_diff($_POST['progress'], $id_array)) ) { // checks if only valid work IDs are being saved
   if ( isset($_POST['username']) && in_array($_POST['username'], $user_array) ) { // checks if user is valid
-
+    
     $progress = $_POST['progress'];
     $user = $_POST['username'];
     
     $handle = fopen("data/progress/$user", "w");
     fwrite($handle, implode(',', $progress));
-      
+    
+    setcookie("user", $user); // always return to user that the work has been saved for to avoid confusion
   }
 }
 
