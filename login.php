b3c875d97b1a0517cf7f84882fa0fc7d44672b18
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2020-12-02 21:52:45 +0100

    Removed obsolete strings from login.php

--- login.php
+++ login.php
@@ -8,7 +8,6 @@ if(isset($_POST['password'])) {
   $key=file_get_contents('password.txt');
 
   if ($password == $key) {
-    echo "Success";
     
     if (isset($_SESSION['origin'])) {
       header("Location: ".$_SESSION['origin']);
@@ -20,7 +19,6 @@ if(isset($_POST['password'])) {
     $_SESSION['login_successful'] = true;
   }
   else {
-    echo "Wrong password";
     header("Location: index.php");
     $_SESSION['login_successful'] = false;
     $_SESSION['return_to_login'] = true; // tells login screen this is no refresh
