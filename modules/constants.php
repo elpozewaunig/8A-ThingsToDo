7871eb6cd8dd3507f2abb133bb14a964c9e11787
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2021-01-27 18:24:07 +0100

    Split INFD and INFH up into groups

--- modules/constants.php
+++ modules/constants.php
@@ -1,5 +1,5 @@
 <?php
 
-const SUBJECTS = ["KV", "VWA", "M", "D", "E", "GSPB", "GWK", "PUP", "BIUK", "CH", "PH", "CHL", "ME", "BE", "L", "IT", "CAE", "REK", "REE", "BSPK", "BSPM", "INFD", "INFH", "WPF-SPA", "WPF-PUP", "WPF-ME", "WPF-GSPB", "WPF-BIUK", "WPF-SPKU"];
+const SUBJECTS = ["KV", "VWA", "M", "D", "E", "GSPB", "GWK", "PUP", "BIUK", "CH", "PH", "CHL", "ME", "BE", "L", "IT", "CAE", "REK", "REE", "BSPK", "BSPM", "INFD 1", "INFD 2", "INFH 1", "INFH 2", "WPF-SPA", "WPF-PUP", "WPF-ME", "WPF-GSPB", "WPF-BIUK", "WPF-SPKU"];
 
 ?>
