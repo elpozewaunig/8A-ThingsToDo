2298c4325779b8ebc055af5a8896b0a3596b3f44
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2021-02-17 21:55:45 +0100

    Properly handle pre- or suffixed subjects in groups

--- modules/get_subjects.php
+++ modules/get_subjects.php
@@ -9,7 +9,8 @@ function get_subjects($subjects_array) { // contains [groups] and subjects
     if (preg_match('/\[.*?\]/', $element)) {  // regex matches everything in []
       $group_name = trim($element, '[] ');
 
-      $group_subjects = str_getcsv( str_replace (' ', '', file_get_contents("data/groups/$group_name")), ',');
+      $group_subjects = str_getcsv(file_get_contents("data/groups/$group_name"), ',');
+      $group_subjects = array_map('trim', $group_subjects);
       
       $subjects = array_merge($subjects, $group_subjects);
     }
