c84e9a4b4986971ca8b0be5872d21fc74f514ab4
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2022-01-19 16:14:44 +0100

    Prevent zoom into password field

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if (isset($_SESSION['login_successful'])) {
 <html>
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 
 <title> <?= title ?> - <?= subtitle ?> </title>
 
