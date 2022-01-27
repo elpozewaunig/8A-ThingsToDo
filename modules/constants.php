d7e951023f596cbc8e71e59d40061db2ba4efa0f
elpozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2022-01-27 23:51:55 +0100

    Added VWA as a subject

--- modules/constants.php
+++ modules/constants.php
@@ -1,5 +1,5 @@
 <?php
 
-const SUBJECTS = ["KV", "M", "D", "E", "GSPB", "GWK", "PUP", "BIUK", "CH", "PH", "ME", "BE", "L", "IT", "REK", "REE", "BSPK", "BSPM", "INF-KAM", "INF-WER", "WPF-SPA", "KV (A)", "M (A)", "D (A)", "E (A)", "GSPB (A)", "GWK (A)", "PUP (A)", "BIUK (A)", "CH (A)", "PH (A)", "ME (A)", "BE (A)", "L (A)", "IT (A)", "REK (A)", "REE (A)", "BSPK (A)", "BSPM (A)", "INF-KAM (A)", "INF-WER (A)", "WPF-SPA (A)", "KV (B)", "M (B)", "D (B)", "E (B)", "GSPB (B)", "GWK (B)", "PUP (B)", "BIUK (B)", "CH (B)", "PH (B)", "ME (B)", "BE (B)", "L (B)", "IT (B)", "REK (B)", "REE (B)", "BSPK (B)", "BSPM (B)", "INF-KAM (B)", "INF-WER (B)", "WPF-SPA (B)"];
+const SUBJECTS = ["KV", "VWA", "M", "D", "E", "GSPB", "GWK", "PUP", "BIUK", "CH", "PH", "ME", "BE", "L", "IT", "REK", "REE", "BSPK", "BSPM", "INF-KAM", "INF-WER", "WPF-SPA", "KV (A)", "M (A)", "D (A)", "E (A)", "GSPB (A)", "GWK (A)", "PUP (A)", "BIUK (A)", "CH (A)", "PH (A)", "ME (A)", "BE (A)", "L (A)", "IT (A)", "REK (A)", "REE (A)", "BSPK (A)", "BSPM (A)", "INF-KAM (A)", "INF-WER (A)", "WPF-SPA (A)", "KV (B)", "M (B)", "D (B)", "E (B)", "GSPB (B)", "GWK (B)", "PUP (B)", "BIUK (B)", "CH (B)", "PH (B)", "ME (B)", "BE (B)", "L (B)", "IT (B)", "REK (B)", "REE (B)", "BSPK (B)", "BSPM (B)", "INF-KAM (B)", "INF-WER (B)", "WPF-SPA (B)"];
 
 ?>
